30d113b8e2496deffd2ab53de0468c5df84d58a8
DANISH <124383478+Mddanish4338@users.noreply.github.com>
2025-02-25 01:02:52 +0530

    Update firebase.jsx

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -9,13 +9,7 @@ import {
 import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDUiFNv9S-I05EGFQUtlOR86Y7J5Z6xs0U",
-  authDomain: "prototype-724a3.firebaseapp.com",
-  projectId: "prototype-724a3",
-  storageBucket: "prototype-724a3.firebasestorage.app",
-  messagingSenderId: "1080404850903",
-  appId: "1:1080404850903:web:83f24a37b0d03a88761892",
-  measurementId: "G-9HDGDWTV5W",
+
 };
 
 const app = initializeApp(firebaseConfig);
